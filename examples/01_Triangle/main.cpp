7c165439aa7f2a2002229b3ef413735f862fa353
LJHG <674478778@qq.com>
2022-05-24 16:43:40 +0800

    store main.cpp to examples/01_Triangle

--- examples/01_Triangle/main.cpp
+++ examples/01_Triangle/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+//
+// Created by lm on 2022/5/24.
+//
+
 #include "WindowApp.h"
-#include "CommonUtils.h"
 #include "MathUtils.h"
 #include "Renderer.h"
 #include "ShadingPipeline.h"
@@ -11,10 +13,10 @@ const int WIDTH = 640, HEIGHT = 480; // SDL窗口的宽和高
 int main() {
     MicroRenderer::WindowApp app(WIDTH,HEIGHT,"first window");
     MicroRenderer::VertexData v1,v2,v3,v4,v5,v6;
-    //triangle 1 绿色在前
-    v1.position = glm::vec3(0,1,0); v1.color=glm::vec4 (255,128,0,0);
-    v2.position = glm::vec3(1,0,0); v2.color=glm::vec4 (0,128,0,0);
-    v3.position = glm::vec3(-1,-1,0); v3.color=glm::vec4 (0,128,0,0);
+    //triangle 1 彩色在前
+    v1.position = glm::vec3(0,1,0); v1.color=glm::vec4 (255,0,0,0);
+    v2.position = glm::vec3(1,0,0); v2.color=glm::vec4 (0,255,0,0);
+    v3.position = glm::vec3(-1,-1,0); v3.color=glm::vec4 (0,0,255,0);
     //triangle 2
     v4.position = glm::vec3(-1,1,1); v4.color=glm::vec4 (0,0,128,0);
     v5.position = glm::vec3(0,-1,1); v5.color=glm::vec4 (0,0,128,0);
@@ -32,18 +34,6 @@ int main() {
         pipeline.shade(SIMPLE_SHADER,FILL);
         uint8_t* pixels = pipeline.getResult();
         app.updateCanvas(pixels,WIDTH,HEIGHT, 3);
-
-//        MicroRenderer::Image image = MicroRenderer::CommonUtils::loadImage("../assets/1.png");
-//        uint8_t pixels[WIDTH*HEIGHT*3];
-//        for(int i=0;i<HEIGHT;i++){
-//            for(int j=0;j<WIDTH;j++){
-//                int index = (i*WIDTH+j)*3;
-//                pixels[index+0] = 128;
-//                pixels[index+1] = 128;
-//                pixels[index+2] = 128;
-//            }
-//        }
-//        app.updateCanvas(pixels,WIDTH,HEIGHT, 3);
     }
 
     return 0;
