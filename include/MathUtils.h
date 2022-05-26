bcc40f1a3b5d20f4796015e2841403dd1c2ab506
LJHG <674478778@qq.com>
2022-05-26 16:34:01 +0800

    add rotationByX

--- include/MathUtils.h
+++ include/MathUtils.h
@@ -15,18 +15,28 @@ namespace MicroRenderer{
     /**
      * math calculation class, provide functions such as:
      * 1. model, view, projection matrix calculation and so on...
-     * 2. interpolation related function
-     * 3. judge if a pixel is inside a triangle ...
+     * 2. model transformation (rotation, translation)...
+     * 3. interpolation related function
+     * 4. judge if a pixel is inside a triangle ...
      */
     class MathUtils {
     public:
+        /*** matrix calculation ***/
         static glm::mat4 calViewMatrix(glm::vec3 cameraPos, glm::vec3 target, glm::vec3 worldUp);
         static glm::mat4 calOrthoProjectionMatrix(float fov, float aspectRatio, float zNear, float zFar);
         static glm::mat4 calPerspectiveProjectionMatrix(float fov, float aspectRatio, float zNear, float zFar);
         static glm::mat4 calViewPortMatrix(int left, int top, int width, int height);
+
+        /*** model transformation ***/
+        static glm::mat4 rotationByX(const glm::mat4 matrix, const double angle);
+
+
+        /*** interpolation ***/
         static VertexOutData lerp(VertexOutData from, VertexOutData to, float t); // interpolation for VertexOut data, for line drawing
         static VertexOutData barycentricLerp(int x, int y, VertexOutData v1, VertexOutData v2, VertexOutData v3); // interpolation for VertexOut data, for fill drawing
         static std::tuple<float, float, float> computeBarycentric2D(float x, float y, float x1, float y1, float x2, float y2, float x3, float y3);
+
+        /*** trivial ***/
         static bool insideTriangle(float x, float y, float x1, float y1, float x2, float y2, float x3, float y3);
         static float crossProduct(float a1, float a2, float b1, float b2);
 
