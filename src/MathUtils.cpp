3a1df18d651b40f810afdcb24dfba46154551345
LJHG <674478778@qq.com>
2022-05-26 12:47:17 +0800

    add copied and self-implemented view and projection matrix calculation

--- src/MathUtils.cpp
+++ src/MathUtils.cpp
@@ -7,49 +7,73 @@
 
 namespace MicroRenderer{
 
-    glm::mat4 MathUtils::calViewMatrix(glm::vec3 cameraPos, glm::vec3 target, glm::vec3 worldUp){
-        //copy from https://github.com/ZeusYang/TinySoftRenderer/blob/master/src/TRMathUtils.cpp
-        glm::mat4 vMat;
-        glm::vec3 zAxis = glm::normalize(cameraPos - target);
-        glm::vec3 xAxis = glm::normalize(glm::cross(worldUp, zAxis));
-        glm::vec3 yAxis = glm::normalize(glm::cross(zAxis, xAxis));
+    /**** copy view matrix and projection matrix ****/
+//    glm::mat4 MathUtils::calViewMatrix(glm::vec3 cameraPos, glm::vec3 target, glm::vec3 worldUp){
+//        //copy from https://github.com/ZeusYang/TinySoftRenderer/blob/master/src/TRMathUtils.cpp
+//        std::cout<<"using copy view matrix"<<std::endl;
+//        glm::mat4 vMat;
+//        glm::vec3 zAxis = glm::normalize(cameraPos - target);
+//        glm::vec3 xAxis = glm::normalize(glm::cross(worldUp, zAxis));
+//        glm::vec3 yAxis = glm::normalize(glm::cross(zAxis, xAxis));
+//
+//        vMat[0][0] = xAxis.x; vMat[0][1] = yAxis.x; vMat[0][2] = zAxis.x; vMat[0][3] = 0.0f;
+//        vMat[1][0] = xAxis.y; vMat[1][1] = yAxis.y; vMat[1][2] = zAxis.y; vMat[1][3] = 0.0f;
+//        vMat[2][0] = xAxis.z; vMat[2][1] = yAxis.z; vMat[2][2] = zAxis.z; vMat[2][3] = 0.0f;
+//        vMat[3][0] = -glm::dot(xAxis, cameraPos);
+//        vMat[3][1] = -glm::dot(yAxis, cameraPos);
+//        vMat[3][2] = -glm::dot(zAxis, cameraPos);
+//        vMat[3][3] = 1.0f;
+//
+//        return vMat;
+//    }
 
-        vMat[0][0] = xAxis.x; vMat[0][1] = yAxis.x; vMat[0][2] = zAxis.x; vMat[0][3] = 0.0f;
-        vMat[1][0] = xAxis.y; vMat[1][1] = yAxis.y; vMat[1][2] = zAxis.y; vMat[1][3] = 0.0f;
-        vMat[2][0] = xAxis.z; vMat[2][1] = yAxis.z; vMat[2][2] = zAxis.z; vMat[2][3] = 0.0f;
-        vMat[3][0] = -glm::dot(xAxis, cameraPos);
-        vMat[3][1] = -glm::dot(yAxis, cameraPos);
-        vMat[3][2] = -glm::dot(zAxis, cameraPos);
-        vMat[3][3] = 1.0f;
+//    glm::mat4 MathUtils::calPerspectiveProjectionMatrix(float fov, float aspectRatio, float zNear, float zFar)
+//    {
+//        //copy from https://github.com/ZeusYang/TinySoftRenderer/blob/master/src/TRMathUtils.cpp
+//        //Setup perspective matrix (camera space -> homogeneous space)
+//        std::cout<<"using copy projection matrix"<<std::endl;
+//        glm::mat4 pMat = glm::mat4(1.0f);
+//
+//        float rFovy = fov * 3.14159265358979323846 / 180;
+//        const float tanHalfFovy = std::tan(rFovy * 0.5f);
+//        float f_n = zFar - zNear;
+//        pMat[0][0] = 1.0f / (aspectRatio*tanHalfFovy); pMat[0][1] = 0.0f;				pMat[0][2] = 0.0f;					pMat[0][3] = 0.0f;
+//        pMat[1][0] = 0.0f;						  pMat[1][1] = 1.0f / tanHalfFovy;  pMat[1][2] = 0.0f;					pMat[1][3] = 0.0f;
+//        pMat[2][0] = 0.0f;						  pMat[2][1] = 0.0f;			    pMat[2][2] = -(zFar + zNear) / f_n;	pMat[2][3] = -1.0f;
+//        pMat[3][0] = 0.0f;						  pMat[3][1] = 0.0f;				pMat[3][2] = -2.0f*zNear*zFar / f_n;	pMat[3][3] = 0.0f;
+//        return pMat;
+//    }
+
+    /**** self implement view matrix and projection matrix ****/
+    glm::mat4 MathUtils::calViewMatrix(glm::vec3 cameraPos, glm::vec3 target, glm::vec3 worldUp){
+        //self implementing -> according to games101 p3
+        std::cout<<"using self implement view matrix"<<std::endl;
+        glm::mat4 vMat;
+
+        glm::mat4 rotation(1.0f);
+        glm::mat4 translation(1.0f);
+
+        glm::vec3 g_cross_t = glm::cross(target,worldUp);
+
+        // rotation matrix
+        //first row
+        rotation[0][0] = g_cross_t[0]; rotation[1][0] = g_cross_t[1]; rotation[2][0] = g_cross_t[2];
+        //second row
+        rotation[0][1] = worldUp[0]; rotation[1][1] = worldUp[1]; rotation[2][1] = worldUp[2];
+        //third row
+        rotation[0][2] = -target[0]; rotation[1][2] = -target[1]; rotation[2][2] = -target[2];
+
+        //translation matrix
+        // last column
+        translation[3][0] = -cameraPos[0]; translation[3][1] = -cameraPos[1]; translation[3][2] = -cameraPos[2];
+        vMat = rotation * translation;
 
         return vMat;
     }
 
-    glm::mat4 MathUtils::calOrthoProjectionMatrix(float fov, float aspectRatio, float zNear, float zFar) {
-        float n = zNear;
-        float f = zFar;
-        float t = -tan( (fov/360)*PI ) * abs(n);
-        float b = t * (-1);
-        float r = aspectRatio * t;
-        float l = r * (-1);
-
-        //  matrix for orthographic
-        glm::mat4 orthoScale = glm::mat4(1.0f);
-        glm::mat4 orthoTrans = glm::mat4(1.0f);
-        orthoScale[0][0] = 2/(r-l);
-        orthoScale[1][1] = 2/(t-b);
-        orthoScale[2][2] = 2/(n-f);
-
-        orthoTrans[0][3] = (-1) * (r+l)/2;
-        orthoTrans[1][3] = (-1) * (t+b)/2;
-        orthoTrans[2][3] = (-1) * (n+f)/2;
-
-        glm::mat4 orthoProjectionMatrix = orthoScale * orthoTrans;
-
-        return orthoProjectionMatrix;
-    }
-
-    glm::mat4 MathUtils::calPerspectiveProjectionMatrix(float fov, float aspectRatio, float zNear, float zFar){
+        glm::mat4 MathUtils::calPerspectiveProjectionMatrix(float fov, float aspectRatio, float zNear, float zFar){
+        //copy from games101
+        std::cout<<"using self implement projection matrix"<<std::endl;
         float n = zNear;
         float f = zFar;
         float t = -tan( (fov/360)*PI ) * abs(n);
@@ -64,14 +88,42 @@ namespace MicroRenderer{
         glm::mat4 pers2Ortho = glm::mat4(0);
         pers2Ortho[0][0] = n;
         pers2Ortho[1][1] = n;
-        pers2Ortho[3][2] = 1;
+        pers2Ortho[2][3] = 1;
         pers2Ortho[2][2] = n+f;
-        pers2Ortho[2][3] = (-1)*n*f;
+        pers2Ortho[3][2] = (-1)*n*f;
 
         // cal projection matrix
         glm::mat4 perspectiveProjectionMatrix = orthoProjectionMatrix * pers2Ortho;
         return perspectiveProjectionMatrix;
     }
+
+
+    glm::mat4 MathUtils::calOrthoProjectionMatrix(float fov, float aspectRatio, float zNear, float zFar) {
+        // copy from games101
+        float n = zNear;
+        float f = zFar;
+        float t = -tan( (fov/360)*PI ) * abs(n);
+        float b = t * (-1);
+        float r = aspectRatio * t;
+        float l = r * (-1);
+
+        //  matrix for orthographic
+        glm::mat4 orthoScale = glm::mat4(1.0f);
+        glm::mat4 orthoTrans = glm::mat4(1.0f);
+        orthoScale[0][0] = 2/(r-l);
+        orthoScale[1][1] = 2/(t-b);
+        orthoScale[2][2] = 2/(n-f);
+
+        orthoTrans[3][0] = (-1) * (r+l)/2;
+        orthoTrans[3][1] = (-1) * (t+b)/2;
+        orthoTrans[3][2] = (-1) * (n+f)/2;
+
+        glm::mat4 orthoProjectionMatrix = orthoScale * orthoTrans;
+
+        return orthoProjectionMatrix;
+    }
+
+
 
     glm::mat4 MathUtils::calViewPortMatrix(int left, int top, int width, int height) {
         // the matrix in glm in stored by column... , so matrix[x][y] access the element of x col ,y row
