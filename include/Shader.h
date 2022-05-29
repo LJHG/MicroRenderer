5c6ca8d00f7c1e4b8adb3e8f4d0f1f1607e1e41c
LJHG <674478778@qq.com>
2022-05-29 22:49:09 +0800

    [refract] add Gouraud shader and refract shader 子类里写的函数外部访问不到，原因是shadingPipeline的成员变量类型是Shader而不是子类，所以只好把所有的Shader可能用到的函数和变量都放进基类里了(无奈之举),然后不同的子类给取所需，子类只需要实现vertexShader和fragmentShader即可。

--- include/Shader.h
+++ include/Shader.h
@@ -8,6 +8,7 @@
 #include "Structure.h"
 #include <iostream>
 #include "LogUtils.h"
+#include "Light.h"
 
 namespace MicroRenderer{
     class Shader {
@@ -19,23 +20,50 @@ namespace MicroRenderer{
         void setViewMatrix(glm::mat4 matrix);
         void setProjectionMatrix(glm::mat4 matrix);
 
+        //Gouraud and phong shader needed
+        void setMaterial(Material _material);
+        void setEyePos(glm::vec3 _eyePos);
+        void addDirectionLight(DirectionLight light);
+        void setTexture(std::string _textureUrl);
+
     protected:
         glm::mat4 modelMatrix;
         glm::mat4 viewMatrix;
         glm::mat4 projectionMatrix;
+
+        //Gouraud and phong shader needed
+        std::string textureUrl;
+        std::vector<DirectionLight> directionLights; // multiple lights
+        Material material;
+        glm::vec3 eyePos;
     };
 
-    // simple shader, just pass data, didn't do anything
+    /**
+     *  simple shader, just pass data, didn't do anything
+     */
+
     class SimpleShader: public Shader{
     public:
         VertexOutData vertexShader(VertexData &v) override;
         glm::vec4 fragmentShader(VertexOutData &v) override;
     };
 
-    // 3d shader, implement mvp transformation for vertex shader, fragment shader just pass color
+    /**
+     * 3d shader, implement mvp transformation for vertex shader, fragment shader just pass color
+     */
     class ThreeDShader: public Shader{
     public:
         ThreeDShader(glm::mat4 _modelMatrix, glm::mat4 _viewMatrix, glm::mat4 _projectionMatrix);
+        VertexOutData vertexShader(VertexData &v) override;
+        glm::vec4 fragmentShader(VertexOutData &v) override;
+    };
+
+    /**
+     * Gouraud shader, DO NOT interpolate normal, so calculate color at vertexShader stage and interpolate color
+     */
+    class GouraudShader : public Shader{
+    public:
+        GouraudShader(glm::mat4 _modelMatrix,glm::mat4 _viewMatrix,glm::mat4 _projectionMatrix);
         VertexOutData vertexShader(VertexData &v) override;
         glm::vec4 fragmentShader(VertexOutData &v) override;
     };
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -17,6 +17,21 @@ namespace MicroRenderer{
     void Shader::setProjectionMatrix(glm::mat4 matrix) {
         projectionMatrix = matrix;
     }
+    void Shader::setMaterial(Material _material) {
+        material = _material;
+    }
+
+    void Shader::setEyePos(glm::vec3 _eyePos) {
+        eyePos = _eyePos;
+    }
+
+    void Shader::addDirectionLight(DirectionLight light) {
+        directionLights.push_back(light);
+    }
+
+    void Shader::setTexture(std::string _textureUrl) {
+        textureUrl = _textureUrl;
+    }
 
     /**** Simple Shader ****/
 
@@ -57,4 +72,56 @@ namespace MicroRenderer{
         result = v.color;
         return result;
     }
+
+    /**** Gouraud Shader****/
+    GouraudShader::GouraudShader(glm::mat4 _modelMatrix,
+                                 glm::mat4 _viewMatrix,
+                                 glm::mat4 _projectionMatrix) {
+        modelMatrix = _modelMatrix;
+        viewMatrix = _viewMatrix;
+        projectionMatrix = _projectionMatrix;
+        //
+        directionLights.clear();
+        textureUrl = "none";
+    }
+
+    VertexOutData GouraudShader::vertexShader(VertexData &v) {
+        VertexOutData vod;
+        vod.position = glm::vec4(v.position,1.0f);
+        vod.position = projectionMatrix * viewMatrix * modelMatrix * vod.position; //mvp transformation
+        vod.normal = v.normal;
+
+        // 计算着色
+        glm::vec3 ka = material.ka;
+        glm::vec3 kd = material.kd;
+        if(textureUrl != "none"){
+            glm::vec3 kd = material.kd;
+        }
+        glm::vec3 ks = material.ks;
+
+        glm::vec3 color(0.0f,0.0f,0.0f);
+
+        //directional lights
+        for(const auto& light:directionLights){
+            glm::vec3 lightDir = glm::normalize(-light.direction);
+            glm::vec3 viewDir = glm::normalize(eyePos - v.position);
+            glm::vec3 half = glm::normalize(lightDir + viewDir); // 半程向量
+            glm::vec3 L_a = light.ambient * ka;
+            glm::vec3 L_d = light.diffuse * kd * std::max(0.0f,glm::dot(v.normal,lightDir));
+            glm::vec3 L_s = light.specular * ks * std::max(0.0f,glm::dot(v.normal,half));
+
+            color += (L_a + L_d + L_s);
+        }
+        vod.color = glm::vec4(color,1.0f)*255.0f;
+        LogUtils::log(vod.color);
+        return vod;
+    }
+
+    glm::vec4 GouraudShader::fragmentShader(VertexOutData &v) {
+        return v.color;
+    }
+
+
+
+
 }
