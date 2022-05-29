fd5c4e7f571463341d63cb99eff143e210c4eb07
LJHG <674478778@qq.com>
2022-05-29 17:13:09 +0800

    delete log

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -44,16 +44,9 @@ namespace MicroRenderer{
     }
 
     VertexOutData ThreeDShader::vertexShader(VertexData &v) {
-        //std::cout<<"####### view matrix #######"<<std::endl;
-        //LogUtils::log(viewMatrix);
-        //std::cout<<"###########################"<<std::endl;
         VertexOutData vod;
         vod.position = glm::vec4(v.position,1.0f);
-        //std::cout<<"转换前"<<std::endl;
-        //std::cout<<vod.position[0]<<" "<<vod.position[1]<<" "<<vod.position[2]<<" "<<vod.position[3]<<std::endl;
         vod.position = projectionMatrix * viewMatrix * modelMatrix * vod.position; //mvp transformation
-        //std::cout<<"转换后"<<std::endl;
-        //std::cout<<vod.position[0]<<" "<<vod.position[1]<<" "<<vod.position[2]<<" "<<vod.position[3]<<std::endl;
         vod.color= v.color;
         vod.normal = v.normal;
         return vod;
